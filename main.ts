dda3c37b28996636485cba37734d9ed273fa2bed
1711005 <104809338+1711005@users.noreply.github.com>
2022-05-20 10:27:40 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,12 @@
+music.setVolume(255)
+basic.forever(function () {
+    music.playTone(131, music.beat(BeatFraction.Half))
+    music.playTone(175, music.beat(BeatFraction.Half))
+    music.playTone(247, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Quarter))
+    music.playTone(440, music.beat(BeatFraction.Quarter))
+    music.playTone(587, music.beat(BeatFraction.Quarter))
+    music.playTone(185, music.beat(BeatFraction.Quarter))
+    music.playTone(185, music.beat(BeatFraction.Quarter))
+    music.playTone(185, music.beat(BeatFraction.Quarter))
+})
